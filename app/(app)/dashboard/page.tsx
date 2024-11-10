a0b100e4f8e5f9096a33a80e04b63b6ad06b8e7d
p-k-pavan <pkpavan2003@email.com>
2024-11-10 20:16:21 +0530

    #

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -127,11 +127,11 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-screen overflow-hidden">
       <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
       <div className="mb-4">
         <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>
-        <div className="flex items-center">
+        <div className="flex items-center mx-[70%]">
           <input type="text" value={profileUrl} disabled className="input input-bordered w-full p-2 mr-2" />
           <button onClick={copyToClipboard} className="btn btn-outline">
             Copy
